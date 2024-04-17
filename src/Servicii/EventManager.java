178f69a9868f238455a473800834f8cb4e4a8a2b
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2024-04-17 11:42:02 +0300

    move service to manager

--- src/Servicii/EventManager.java
+++ src/Servicii/EventManager.java
@@ -1,4 +1,22 @@
 package Servicii;
 
+import Modele.Eveniment;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventManager {
+    public List<Eveniment> getEvenimenteAzi(List<Eveniment> evenimente) {
+        List<Eveniment> rezultat = new ArrayList<>();
+        LocalDate dataCurenta = LocalDate.now();
+
+        for (Eveniment ev : evenimente) {
+            if (ev.getDataStart().isEqual(dataCurenta)) {
+                rezultat.add(ev);
+            }
+        }
+
+        return rezultat;
+    }
 }
